8e099c2492552a38424792e710a5a732b339659b
0SUI <cnr38332@gmail.com>
2024-10-06 18:53:13 +0900

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-onst { Client, GatewayIntentBits } = require('discord.js');
-const { Guilds, GuildMessages, MessageContent } = GatewayIntentBits;
-const client = new Client({ intents: [Guilds, GuildMessages, MessageContent] });
-client.login('token');
-
-client.once('ready', () => console.log(client.user.tag+' 준비 완료!'));
